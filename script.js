be7613aec1c9468179b29fa073ae03d2f2084ec1
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-15 06:07:03 +0100

    styling app

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let url = "https://api.openweathermap.org/data/2.5/weather?lat={7.376736}&lon={3.939786}&appid={a67a9ac089f9743696b7e7fdeaddd2e9}"
+let apiId = "a67a9ac089f9743696b7e7fdeaddd2e9",
+city = "ibadan";
 
-fetch(url).then(res => res.json()).then(result => console.log(result));
+let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}`;
+
+fetch(api).then(res => res.json()).then(result => console.log(result));
