adb491e9d391fa7dd6a68fbeb87b03e90df7a7c0
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-16 02:33:12 +0100

    styling desktop view

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ city = "ibadan";
 let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}`;
 
 fetch(api).then(res => res.json()).then(result => console.log(result));
+
+
+fetch('https://test.api.amadeus.com/GLIHsZmZGFD9yrCRqjPWajJJ6NGBUEQ1')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
