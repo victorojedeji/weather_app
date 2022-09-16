4376d9e20d69d175300bb100c3dbe3bc1f8ec365
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-16 11:37:28 +0100

    setting up search suggestion

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-let apiId = "a67a9ac089f9743696b7e7fdeaddd2e9",
-city = "ibadan";
+const searchInput = document.querySelector('#search');
+let url = 'https://api.teleport.org/api/cities/?search=';
 
-let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}`;
+searchInput.addEventListener("input", async (e) => {
+        let inputVal = e.target.value;
+        let endPoint = url + inputVal;
+        let result = await(await fetch(endPoint)).json();
+        let cities = result._embedded["city:search-results"];
+        let length = cities.length > 10 ? 10 : cities.length;
+        console.log(cities)
+        for(let j = 0; j < cities.length; j++) {
+           console.log(cities)
+        }
+        console.log(result);
+});
 
-fetch(api).then(res => res.json()).then(result => console.log(result));
 
 
-fetch('https://test.api.amadeus.com/GLIHsZmZGFD9yrCRqjPWajJJ6NGBUEQ1')
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let apiId = "a67a9ac089f9743696b7e7fdeaddd2e9",
+// city = "ibadan";
+
+// let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}`;
+
+// fetch(api).then(res => res.json()).then(result => console.log(result));
